a47c4d75832a45534a0c8c30303212690d1335a1
mscherbinin <mmscherbinin@gmail.com>
2019-02-24 20:17:42 +0300

    Задание 4(task 4)

--- src/task4.java
+++ src/task4.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+//Ввести с консоли число в бинарном формате. Перевести его в int и вывести на экран
 public class task4 {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
