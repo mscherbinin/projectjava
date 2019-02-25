fd826a0720d568ae5ca14682cb44334f1b535301
mscherbinin <mmscherbinin@gmail.com>
2019-02-25 22:30:47 +0300

    Задание 5(task 5)

--- src/task5.java
+++ src/task5.java
@@ -1,5 +1,19 @@
-public class task5 {
-    public static void main(String args[]){
+import java.util.Scanner;
 
-    }
-}
+class task5{
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число ");
+        String m = scan.nextLine();
+        int s = Integer.parseInt(m);
+        System.out.println("1x"+s+"="+1*+s);
+        System.out.println("2x"+s+"="+2*+s);
+        System.out.println("3x"+s+"="+3*+s);
+        System.out.println("4x"+s+"="+4*+s);
+        System.out.println("5x"+s+"="+5*+s);
+        System.out.println("6x"+s+"="+6*+s);
+        System.out.println("7x"+s+"="+7*+s);
+        System.out.println("8x"+s+"="+8*+s);
+        System.out.println("9x"+s+"="+9*+s);
+        System.out.println("10x"+s+"="+10*+s);
+    }}
