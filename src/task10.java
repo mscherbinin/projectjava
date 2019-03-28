3886bdf57bcc4deccc817d3055f472721cc8470b
mscherbinin <mmscherbinin@gmail.com>
2019-03-28 20:51:31 +0300

    Задание 9-10(task 9-10)

--- src/task10.java
+++ src/task10.java
@@ -1,2 +1,22 @@
-public class task10 {
+import java.util.Scanner;
+
+//Произведите ввод данных с клавиатуры в матрицу, а после этого
+//произведите вывод первой строки матрицы на экран, где каждый элемент умножается на 3.
+
+
+public class task10{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] table= new int[3][4];
+        int i, t, k, p = 1;
+        for(i=0; i<3; i++){
+            for(t=0; t<4; t++){
+                System.out.print("Введите значение "+ p++ + " : ");
+                table[i][t] = sc.nextInt();
+            }
+        }
+        for(k=0; k<4; k++){
+            System.out.println(table[0][k]*3);
+        }
+    }
 }
--- src/task9.java
+++ src/task9.java
@@ -3,16 +3,17 @@
 
 import java.util.Scanner;
 
-//public class task9 {
-//    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Введите 3 числа ");
-//        int x = sc.nextInt();
-//        int y = sc.nextInt();
-//        int z = sc.nextInt();
-//        int sample[] = new int[3];
-//        for()
-//
-//    }
-//
-//}
+
+public class task9{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] sample = new int[10];
+        int i;
+        int s = 1;
+        for(i=0; i<10; i++){
+            System.out.print("Введите значение " + s++ +" ");
+                sample[i] = sc.nextInt();}
+        for(i=0; i<10; i++)
+            System.out.println(sample[i]*2);
+    }
+}
