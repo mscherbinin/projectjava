3ea2c75e3bc301d2852a1dacdc5f7e7e97d729da
mscherbinin <mmscherbinin@gmail.com>
2019-03-30 15:26:56 +0300

    Задания 14-15(task 14-15)

--- src/task14.java
+++ src/task14.java
@@ -1,2 +1,25 @@
+
+
+//Необходимо написать программу, которая будет реализовывать следующие действия:
+//        1. Ввод числа с клавиатуры и запись его в строковую переменную S
+//
+//        2. Конвертация строковой переменной S в числовую переменную X типа int
+//
+//        3. Конвертация числа X типа int в число Y типа double
+//
+//        Все три числа разных типов необходимо в столбик вывести на экран
+
+import java.util.Scanner;
+
 public class task14 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число ");
+        String s = sc.nextLine();
+        int x = Integer.parseInt(s);
+        double y = (double) x;
+        System.out.println(s);
+        System.out.println(x);
+        System.out.println(y);
+    }
 }
--- src/task15.java
+++ src/task15.java
@@ -1,2 +1,23 @@
+
+
+//Необходимо написать программу для сортировки данных массива по возрастанию.
+//        Использовать пузырьковый метод сортировки.
+
+
+import java.util.Arrays;
+
 public class task15 {
+    public static void main(String[] args){
+        int[] mass = {25, 2, 5, 12, 11, 100, 3};
+        for(int i = mass.length-1; i>0; i--){
+            for(int j = 0; j<i; j++){
+                if(mass[j]>mass[j+1]){
+                    int tmp = mass[j];
+                    mass[j] = mass[j+1];
+                    mass[j+1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mass));
+    }
 }
